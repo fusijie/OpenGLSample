20528a2450f474c90ac16ef7fb78cce78c224765
Jacky <fusijie@vip.qq.com>
2015-06-17 18:26:44 +0800

    geometry shader point to rectangle...

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -66,11 +66,17 @@ const GLchar* fragmentSource = GLSL(
 
 const GLchar* geometrySource = GLSL(
     layout(points) in;
-    layout(line_strip, max_vertices = 2) out;
+    layout(triangle_strip, max_vertices = 5) out;
     void main(){
-        gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.0, 0.0, 0.0);
+        gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.1, 0.0, 0.0);
         EmitVertex();
-        gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.0, 0.0, 0.0);
+        gl_Position = gl_in[0].gl_Position + vec4(0.1, 0.1, 0.0, 0.0);
+        EmitVertex();
+        gl_Position = gl_in[0].gl_Position + vec4(0.1, -0.1, 0.0, 0.0);
+        EmitVertex();
+        gl_Position = gl_in[0].gl_Position + vec4(-0.1, -0.1, 0.0, 0.0);
+        EmitVertex();
+        gl_Position = gl_in[0].gl_Position + vec4(-0.1, 0.1, 0.0, 0.0);
         EmitVertex();
         EndPrimitive();
     }
