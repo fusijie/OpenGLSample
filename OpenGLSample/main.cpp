984fd4529516d60b5d38f3b16243eedf46de812e
Jacky <fusijie@vip.qq.com>
2015-06-17 10:28:29 +0800

    setup openGL context.

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -1,15 +1,64 @@
 //
 //  main.cpp
-//  OpenGLSample
+//  HelloGLFW
 //
-//  Created by Jacky on 15/6/17.
+//  Created by Jacky on 15/5/6.
 //  Copyright (c) 2015年 Jacky. All rights reserved.
 //
 
+#define GLFW_INCLUDE_GLCOREARB
 #include <iostream>
+#include "glfw3.h"
+
+void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    GLFWwindow* window;
+    
+    glfwSetErrorCallback(error_callback);
+    
+    if(!glfwInit())
+        exit(EXIT_FAILURE);
+    
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    
+    window = glfwCreateWindow(640, 480, "HelloWorld", nullptr, nullptr);
+    
+    if(!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    
+    glfwSetKeyCallback(window, key_callback);
+    
+    while (!glfwWindowShouldClose(window)) {
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
+    exit(EXIT_SUCCESS);
 }
