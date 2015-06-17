1edc817a30c1a79773a41e278c7fa583b294afc5
Jacky <fusijie@vip.qq.com>
2015-06-17 10:19:21 +0800

    create project.

---
+++ OpenGLSample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGLSample
+//
+//  Created by Jacky on 15/6/17.
+//  Copyright (c) 2015年 Jacky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+OpenGL Sample.
+
+---
+
+My OpenGL study notes.
+
+From [open.gl](https://open.gl).
+
+Base on OpenGL 3.2/Mac OSX.
