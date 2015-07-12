896e2aaf118c412409b5709a96da57a26d4ff5af
Jacky <fusijie@vip.qq.com>
2015-07-12 17:33:45 +0800

    Light with Gouraud shading algorithm.

--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -114,40 +114,39 @@ const GLchar* vertexSource =
     "#version 150 core\n"
     "in vec3 position;"
     "in vec3 normal;"
-    "out vec3 Normal;"
-    "out vec3 FragPos;"
+    "out vec3 LightColor;"
     "uniform mat4 model;"
     "uniform mat4 view;"
     "uniform mat4 projection;"
-    "void main() {"
-    "   gl_Position = projection * view * model * vec4(position, 1.0);"
-    "   FragPos = vec3(model * vec4 (position, 1.0));"
-    "   Normal = mat3(transpose(inverse(model))) * normal;"
-    "}";
-
-const GLchar* fragmentSource_object =
-    "#version 150 core\n"
-    "in vec3 Normal;"
-    "in vec3 FragPos;"
-    "out vec4 finalColor;"
-    "uniform vec3 objectColor;"
-    "uniform vec3 lightColor;"
     "uniform vec3 lampPos;"
     "uniform vec3 viewPos;"
+    "uniform vec3 lightColor;"
     "void main() {"
+    "   gl_Position = projection * view * model * vec4(position, 1.0);"
+    "   vec3 positionInWorld = vec3(model * vec4 (position, 1.0));"
+    "   vec3 normalInWorld = mat3(transpose(inverse(model))) * normal;"
     "   float ambientStrength = 0.3f;"
     "   vec3 ambient = ambientStrength * lightColor;"
-    "   vec3 normalDir = normalize(Normal);"
-    "   vec3 lightDir = normalize(lampPos - FragPos);"
+    "   vec3 normalDir = normalize(normalInWorld);"
+    "   vec3 lightDir = normalize(lampPos - positionInWorld);"
     "   float diff = max(dot(normalDir, lightDir), 0.0);"
     "   vec3 diffuse = diff * lightColor;"
     "   float specularStrength = 1.0f;"
-    "   vec3 viewDir = normalize(viewPos - FragPos);"
+    "   vec3 viewDir = normalize(viewPos - positionInWorld);"
     "   vec3 reflectDir = reflect(-lightDir, normalDir);"
     "   float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);"
     "   vec3 specular = specularStrength * spec * lightColor;"
-    "   finalColor = vec4((ambient + diffuse + specular) * objectColor, 1.0);"
-    "}";
+    "   LightColor = ambient + diffuse + specular;"
+"}";
+
+const GLchar* fragmentSource_object =
+    "#version 150 core\n"
+    "in vec3 LightColor;"
+    "out vec4 finalColor;"
+    "uniform vec3 objectColor;"
+    "void main() {"
+    "   finalColor = vec4(LightColor * objectColor, 1.0);"
+"}";
 
 const GLchar* fragmentSource_lamp =
     "#version 150 core\n"
@@ -341,9 +340,6 @@ int main(int argc, const char * argv[]) {
         GLint uniProjection = glGetUniformLocation(shaderProgram[0], "projection");
         glUniformMatrix4fv(uniProjection, 1, GL_FALSE, glm::value_ptr(projection));
         
-        GLint uniObjectColor = glGetUniformLocation(shaderProgram[0], "objectColor");
-        glUniform3f(uniObjectColor, 1.0f, 0.5f, 0.31f);
-        
         GLint uniLightColor = glGetUniformLocation(shaderProgram[0], "lightColor");
         glUniform3f(uniLightColor, 1.0f, 1.0f, 1.0f);
         
@@ -352,6 +348,9 @@ int main(int argc, const char * argv[]) {
         
         GLint uniViewPos = glGetUniformLocation(shaderProgram[0], "viewPos");
         glUniform3f(uniViewPos, camera.Position.x, camera.Position.y, camera.Position.z);
+        
+        GLint uniObjectColor = glGetUniformLocation(shaderProgram[0], "objectColor");
+        glUniform3f(uniObjectColor, 1.0f, 0.5f, 0.31f);
         
         ///Draw.
         glDrawArrays(GL_TRIANGLES, 0, 36);
