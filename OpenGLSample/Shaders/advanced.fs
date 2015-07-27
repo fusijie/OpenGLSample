db2866dfbd697981047421cd90761f4277449921
fusijie <fusijie@vip.qq.com>
2015-07-27 22:33:41 +0800

    linear depth.

--- OpenGLSample/Shaders/advanced.fs
+++ OpenGLSample/Shaders/advanced.fs
@@ -6,6 +6,15 @@ out vec4 finalColor;
 
 uniform sampler2D texture1;
 
+float linearizeDepth(float depth)
+{
+    float near = 0.1;
+    float far = 100.0;
+    float z = depth * 2.0 - 1.0; // Back to NDC
+    return (2.0 * near) / (far + near - z * (far - near));
+}
+
 void main() {
-    finalColor = vec4(vec3(gl_FragCoord.z), 1.0f);
+    float depth = linearizeDepth(gl_FragCoord.z);
+    finalColor = vec4(vec3(depth), 1.0f);
 }
