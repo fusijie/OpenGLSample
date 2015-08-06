6a467ade9d9541c3daca3b1917caf8c0ce338e50
fusijie <fusijie@vip.qq.com>
2015-08-06 22:31:29 +0800

    add framebuffer.

---
+++ OpenGLSample/Shaders/effect.vs
@@ -0,0 +1,11 @@
+#version 150 core
+
+in vec2 position;
+in vec2 texCoords;
+
+out vec2 TexCoords;
+
+void main() {
+    gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+    TexCoords = texCoords;
+}
--- OpenGLSample/main.cpp
+++ OpenGLSample/main.cpp
@@ -152,6 +152,7 @@ int main(int argc, const char * argv[]) {
     
     //Shade operation
     Program program("advanced.vs", "advanced.fs");
+    Program effectProgram("effect.vs", "effect.fs");
     
     //Create data.
     GLfloat cubeVertices[] = {
@@ -209,56 +210,88 @@ int main(int argc, const char * argv[]) {
         5.0f,  -0.5f, -5.0f,  2.0f, 2.0f
     };
     
+    GLfloat quadVertices[] = {   // Vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+        // Positions   // TexCoords
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        -1.0f, -1.0f,  0.0f, 0.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        
+        -1.0f,  1.0f,  0.0f, 1.0f,
+        1.0f, -1.0f,  1.0f, 0.0f,
+        1.0f,  1.0f,  1.0f, 1.0f
+    };
+    
     //Create a VAO and VBO.
-    GLuint vao[2];
-    glGenVertexArrays(2, vao);
-    GLuint vbo[2];
-    glGenBuffers(2, vbo);
+    GLuint vao[3];
+    glGenVertexArrays(3, vao);
+    GLuint vbo[3];
+    glGenBuffers(3, vbo);
     
     //Bind first VAO.
     glBindVertexArray(vao[0]);
-    
-    //Create a VBO, and copy vertices data to it.
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-    
-    //Bind VBO to shader attribute.
     GLint posAttrib_0 = glGetAttribLocation(program.getProgram(), "position");
     glEnableVertexAttribArray(posAttrib_0);
     glVertexAttribPointer(posAttrib_0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), 0);
     GLint texCoordAttrib_0 = glGetAttribLocation(program.getProgram(), "texCoords");
     glEnableVertexAttribArray(texCoordAttrib_0);
     glVertexAttribPointer(texCoordAttrib_0, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)(3*sizeof(GL_FLOAT)));
-    
-    //Bind default VAO.
     glBindVertexArray(0);
     
     //Bind second VAO.
     glBindVertexArray(vao[1]);
-    
-    //Create a VBO, and copy vertices data to it.
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
-    
-    //Bind VBO to shader attribute.
     GLint posAttrib_1 = glGetAttribLocation(program.getProgram(), "position");
     glEnableVertexAttribArray(posAttrib_1);
     glVertexAttribPointer(posAttrib_1, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), 0);
     GLint texCoordAttrib_1 = glGetAttribLocation(program.getProgram(), "texCoords");
     glEnableVertexAttribArray(texCoordAttrib_1);
     glVertexAttribPointer(texCoordAttrib_1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GL_FLOAT), (void*)(3*sizeof(GL_FLOAT)));
+    glBindVertexArray(0);
     
-    //Bind default VAO.
+    //Bind second VAO.
+    glBindVertexArray(vao[2]);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    GLint posAttrib_2 = glGetAttribLocation(effectProgram.getProgram(), "position");
+    glEnableVertexAttribArray(posAttrib_2);
+    glVertexAttribPointer(posAttrib_2, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GL_FLOAT), 0);
+    GLint texCoordAttrib_2 = glGetAttribLocation(effectProgram.getProgram(), "texCoords");
+    glEnableVertexAttribArray(texCoordAttrib_2);
+    glVertexAttribPointer(texCoordAttrib_2, 2, GL_FLOAT, GL_FALSE, 4*sizeof(GL_FLOAT), (void*)(2*sizeof(GL_FLOAT)));
     glBindVertexArray(0);
     
     //Load textures
     GLuint cubeTexture = loadTexture("pattern4diffuseblack.jpg");
     GLuint floorTexture = loadTexture("metal.png");
     
-    //GLState
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-//    glDepthFunc(GL_ALWAYS);
+    //Create framebuffer
+    GLuint framebuffer;
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    
+    GLuint texColorBuffer;
+    glGenTextures(1, &texColorBuffer);
+    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+    
+    GLuint rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, WIDTH, HEIGHT);
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+    
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        std::cout << "ERROR: FRAMEBUFFER is not completed." << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
     
     while (!glfwWindowShouldClose(window)) {
         
@@ -267,7 +300,11 @@ int main(int argc, const char * argv[]) {
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         
-        //Clear
+        //Bind framebuffer.
+        glEnable(GL_DEPTH_TEST);
+        glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+        
+        //Clear.
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
@@ -309,6 +346,21 @@ int main(int argc, const char * argv[]) {
         glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
         glDrawArrays(GL_TRIANGLES, 0, 6);
         
+        //Bind framebuffer.
+        glDisable(GL_DEPTH_TEST);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+        
+        //Clear.
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        //Draw.
+        effectProgram.use();
+        
+        glBindVertexArray(vao[2]);
+        glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+        glUniform1d(glGetUniformLocation(effectProgram.getProgram(), "texture1"), 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        
         //Reset.
         glBindTexture(GL_TEXTURE_2D, 0);
         glBindVertexArray(0);
@@ -319,8 +371,11 @@ int main(int argc, const char * argv[]) {
         do_movement();
     }
     
-    glDeleteBuffers(2, vbo);
-    glDeleteVertexArrays(2, vao);
+    glDeleteBuffers(3, vbo);
+    glDeleteVertexArrays(3, vao);
+    glDeleteFramebuffers(1, &framebuffer);
+    glDeleteTextures(1, &texColorBuffer);
+    glDeleteRenderbuffers(1, &rbo);
     
     glfwDestroyWindow(window);
     glfwTerminate();
