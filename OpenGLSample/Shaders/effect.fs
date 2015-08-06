8fe49a3cc7ec3f4f25c2dbf586e6f970a4072a38
fusijie <fusijie@vip.qq.com>
2015-08-07 00:07:39 +0800

    add post-processing.

--- OpenGLSample/Shaders/effect.fs
+++ OpenGLSample/Shaders/effect.fs
@@ -6,6 +6,47 @@ out vec4 finalColor;
 
 uniform sampler2D texture1;
 
-void main() {
-    finalColor = texture(texture1, TexCoords);
+const float offset = 1.0 / 300;
+
+void main()
+{
+    vec2 offsets[9] = vec2[](
+                             vec2(-offset, offset),  // top-left
+                             vec2(0.0f,    offset),  // top-center
+                             vec2(offset,  offset),  // top-right
+                             vec2(-offset, 0.0f),    // center-left
+                             vec2(0.0f,    0.0f),    // center-center
+                             vec2(offset,  0.0f),    // center-right
+                             vec2(-offset, -offset), // bottom-left
+                             vec2(0.0f,    -offset), // bottom-center
+                             vec2(offset,  -offset)  // bottom-right
+                             );
+    
+//    float kernel[9] = float[](
+//                              -1, -1, -1,
+//                              -1,  9, -1,
+//                              -1, -1, -1
+//                              );
+    float kernel[9] = float[](
+                              1,  1,  1,
+                              1, -8,  1,
+                              1,  1,  1
+                              );
+
+//    float kernel[9] = float[](
+//                              1.0 / 16, 2.0 / 16, 1.0 / 16,
+//                              2.0 / 16, 4.0 / 16, 2.0 / 16,
+//                              1.0 / 16, 2.0 / 16, 1.0 / 16  
+//                              );
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(texture1, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    finalColor = vec4(col, 1.0);
 }
