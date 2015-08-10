6a98c3eccf7f2c4c5d8b0631f80025829528fe25
fusijie <fusijie@vip.qq.com>
2015-08-10 23:30:47 +0800

    Skybox with refract.

--- OpenGLSample/Shaders/nanosuit.fs
+++ OpenGLSample/Shaders/nanosuit.fs
@@ -10,7 +10,8 @@ uniform samplerCube skybox;
 
 void main()
 {
+    float ratio = 1.00 / 1.52;
     vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
+    vec3 R = refract(I, normalize(Normal), ratio);
     color = vec4(texture(skybox, R));
 }
